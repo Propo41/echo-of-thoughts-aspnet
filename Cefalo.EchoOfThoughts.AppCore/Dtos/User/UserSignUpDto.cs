28d68c8eee722d1722b183ac46d94e201748995c
Ali Ahnaf <aliahnaf327@gmail.com>
2023-07-21 11:42:29 +0600

    added data validations to dtos

--- Cefalo.EchoOfThoughts.AppCore/Dtos/User/UserSignUpDto.cs
+++ Cefalo.EchoOfThoughts.AppCore/Dtos/User/UserSignUpDto.cs
@@ -1,9 +1,20 @@
-﻿namespace Cefalo.EchoOfThoughts.AppCore.Dtos.User {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cefalo.EchoOfThoughts.AppCore.Dtos.User {
     public class UserSignUpDto {
+        [Required]
+        [StringLength(maximumLength: 20, MinimumLength = 3)]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
+        [StringLength(maximumLength: 30, MinimumLength = 3)]
         public string FullName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [Url]
         public string ProfilePicture { get; set; }
     }
 }
